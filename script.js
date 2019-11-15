fe38e23fe253782f4321e7b96673c9e764f8eed4
kryadchenko <kryadchenko@srgpartnership.com>
2019-11-15 15:21:21 -0800

    dbleCLick

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function plusButton() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
     console.log("This is it!")
    }
+
+function dblClick(){
+    console.log("That's double click!")
+}
