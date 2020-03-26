30b483b7ac837afdd7a64427da0bf43422b0d7be
kryadchenko <kryadchenko@srgpartnership.com>
2020-03-26 16:21:16 -0700

    Clickable

--- visioning/script.js
+++ visioning/script.js
@@ -1,5 +1,21 @@
 $(document).click(function(event) {
+
     var object = $(event.target);
-    console.log(object);
-    object.find('.card-show').toggleClass('card-hide');
+    var div = object.parent().parent().children();
+    //console.log(div.length);
+    //object.toggleClass("show");
+    //object.toggleClass("hide");
+    for (i=0; i<div.length; i++){
+        console.log(div[i].className);
+        var className = div[i].className;
+        if (className === "show") {
+            console.log("This is Show class");
+            div[i].className = "hide";
+           
+        } else if (className === "hide") {
+            console.log("This is Hide class");
+            div[i].className = "show";
+        }
+    }
+
 });
