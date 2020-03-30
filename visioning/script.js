2cf802ae59143ffc41a807f520906c27a8d468c0
kryadchenko <kryadchenko@srgpartnership.com>
2020-03-30 12:17:59 -0700

    Light

--- visioning/script.js
+++ visioning/script.js
@@ -2,28 +2,10 @@ $(document).click(function(event) {
 
     var object = $(event.target);
     var div = object.parent();
-    console.log(div);
-    //object.toggleClass("show");
-    //object.toggleClass("hide");
-    
-    div.toggleClass("hide");
-    div.toggleClass("show");
+    var className = div[0].className;
 
-    /*
-    for (i=0; i<div.length; i++){
-        console.log(div[i].className);
-        var className = div[i].className;
-        if (className === "front") {
-            console.log("This is Show class");
-            div[i].className = "hide";
-            //div[i].style.transform = "rotateY(180deg)";
-           
-        } else if (className === "back") {
-            console.log("This is Hide class");
-            //div[i].className = "show";
-            //div[i].style.transform = "rotateY(180deg)";
-        }
+    if (className === "show" || className == "hide"){
+        div.toggleClass("hide");
+        div.toggleClass("show");
     }
-
-    */
 });
