3d3a85c68b3a074f7b729f355d9b70fda56d4f2f
kryadchenko <kryadchenko@srgpartnership.com>
2020-03-30 17:46:45 -0700

    Cleanup

--- visioning/script.js
+++ visioning/script.js
@@ -1,14 +1,9 @@
 $(document).click(function(event) {
 
     var object = $(event.target);
-    var preClass = object[0].className;
-    console.log(object[0]);
 
-    
     var div = object.parent();
     var className = div[0].className;
-    console.log(div[0]);
-    console.log(div);
 
     if (className === "show" || className == "hide"){
         div.toggleClass("hide");
